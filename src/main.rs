75f629a6057e5c2f455b4f39fc88ca95e257069c
Ptitet <ptitet73@gmail.com>
2024-12-19 23:49:37 +0100

    wip: first version of main, not working

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@
 pub mod eadk;
 pub mod graphics;
 pub mod math;
+pub mod scene;
 
 use eadk::Color;
 use graphics::Buffer;
+use math::Vec3;
+use scene::{Camera, Scene, Sphere};
 
 #[used]
 #[link_section = ".rodata.eadk_app_name"]
-pub static EADK_APP_NAME: [u8; 8] = *b"Numrust\0";
+pub static EADK_APP_NAME: [u8; 9] = *b"Raymarch\0";
 
 #[used]
 #[link_section = ".rodata.eadk_api_level"]
@@ -21,10 +24,35 @@ pub static EADK_APP_API_LEVEL: u32 = 0;
 pub static EADK_APP_ICON: [u8; 4250] = *include_bytes!("../target/icon.nwi");
 
 #[no_mangle]
+#[link_section = ".rodata._eadk_main"]
 pub fn main() {
     let mut buff = Buffer::new();
-    buff.clear(Color { rgb565: 0x001F });
+    buff.clear(Color::from_rgb(255, 255, 255));
     buff.render();
-    // empty main loop
-    loop {}
+
+    buff.clear(Color::from_rgb(0, 0, 0));
+    buff.render();
+
+    let camera = Camera {
+        position: Vec3::new(0., 0., -10.),
+        focal_length: 3.,
+    };
+    let sphere = Sphere {
+        color: Color::from_rgb(255, 50, 100),
+        position: Vec3::new(0., 0., 2.),
+        radius: 2.,
+    };
+    let scene = Scene {
+        camera,
+        background_color: Color::from_rgb(50, 100, 255),
+        sphere,
+    };
+    scene.render(50, &mut buff);
+    buff.render();
+
+    buff.clear(Color::from_rgb(0, 255, 0));
+    buff.render();
+
+    // hang when finished
+    // loop {}
 }
