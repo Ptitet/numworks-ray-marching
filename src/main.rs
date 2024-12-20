a7f060adf6df311c98667507396387531ea02c71
Ptitet <ptitet73@gmail.com>
2024-12-20 09:58:42 +0100

    feat: water-molecule in the scene and render preview for each column

--- src/main.rs
+++ src/main.rs
@@ -24,28 +24,37 @@ pub static EADK_APP_API_LEVEL: u32 = 0;
 pub static EADK_APP_ICON: [u8; 4250] = *include_bytes!("../target/icon.nwi");
 
 #[no_mangle]
-#[link_section = ".rodata._eadk_main"]
 pub fn main() {
     let mut buff = Buffer::new();
     buff.clear(Color::from_rgb(255, 255, 255));
     buff.render();
 
     let camera = Camera {
-        position: Vec3::new(0., 0., 0.),
-        focal_length: 1.,
+        position: Vec3::new(0., 0., -10.),
+        focal_length: 3.,
     };
-    let sphere = Sphere {
+    let sphere1 = Sphere {
         color: Color::from_rgb(255, 0, 0),
-        position: Vec3::new(0., 0., 5.),
+        position: Vec3::new(0., 0., 2.),
         radius: 2.,
+    };
+    let sphere2 = Sphere {
+        color: Color::from_rgb(255, 255, 255),
+        position: Vec3::new(-2., -1., 2.),
+        radius: 1.,
+    };
+    let sphere3 = Sphere {
+        color: Color::from_rgb(255, 255, 255),
+        position: Vec3::new(2., -1., 2.),
+        radius: 1.,
     };
     let scene = Scene {
         camera,
-        background_color: Color::from_rgb(0, 50, 0),
-        sphere,
+        background_color: Color::from_rgb(50, 100, 255),
+        spheres: [sphere1, sphere2, sphere3],
     };
     scene.render(50, &mut buff);
-    buff.render();
+    // buff.render();
 
     // hang when finished
     loop {}
--- src/scene.rs
+++ src/scene.rs
@@ -1,3 +1,5 @@
+use core::f32;
+
 use crate::{
     eadk::{
         display::{HALF_RATIO, SCREEN_HEIGHT, SCREEN_WIDTH},
@@ -30,6 +32,10 @@ impl SceneObject for Sphere {
     }
 }
 
+pub struct Light {
+    pub position: Vec3,
+}
+
 #[derive(Clone, Copy)]
 pub struct Camera {
     pub position: Vec3,
@@ -44,12 +50,26 @@ impl Camera {
 }
 
 pub struct Scene {
-    pub sphere: Sphere,
+    pub spheres: [Sphere; 3],
     pub camera: Camera,
     pub background_color: Color,
+    pub light: Light,
 }
 
 impl Scene {
+    fn march_radius_at(&self, position: Vec3) -> (f32, Sphere) {
+        let mut min_distance = f32::MAX;
+        let mut closest_sphere = self.spheres[0];
+        for sphere in self.spheres {
+            let distance = sphere.distance_with(position);
+            if distance < min_distance {
+                min_distance = distance;
+                closest_sphere = sphere;
+            }
+        }
+        return (min_distance, closest_sphere);
+    }
+
     fn march_ray_for(
         &self,
         x: f32,
@@ -58,21 +78,21 @@ impl Scene {
         camera_sensor_position: Vec3,
     ) -> Color {
         let mut current_position = Vec3::new(x, y, self.camera.position.z);
-        // let start_position = current_position.clone();
+        let start_position = current_position.clone();
         let mut ray_direction = current_position - camera_sensor_position;
         ray_direction.normalize();
         for _ in 0..max_iteration {
-            let march_radius = self.sphere.distance_with(current_position);
+            let (march_radius, closest_sphere) = self.march_radius_at(current_position);
             if march_radius < 0.1 {
-                // let travel_distance = (current_position - start_position).magnitude();
-                return self.sphere.get_ray_color(0.);
+                let travel_distance = (current_position - start_position).magnitude();
+                return closest_sphere.get_ray_color(travel_distance);
             }
             current_position += ray_direction * march_radius;
         }
         return self.background_color;
     }
 
-    pub fn render(self, max_iteration: u16, buff: &mut Buffer) {
+    pub fn render(&self, max_iteration: u16, buff: &mut Buffer) {
         let camera_sensor_position = self.camera.get_sensor_position();
         for screen_x in 0..=SCREEN_WIDTH {
             let x = screen_x as f32 / 240. - HALF_RATIO;
@@ -81,6 +101,7 @@ impl Scene {
                 let color = self.march_ray_for(x, y, max_iteration, camera_sensor_position);
                 buff.set_at(screen_x, screen_y, color);
             }
+            buff.fast_render();
         }
     }
 }
